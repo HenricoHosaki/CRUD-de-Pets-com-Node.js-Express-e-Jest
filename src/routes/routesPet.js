94536c7fc57b838088342ccba53900045c4890b3
HenricoHosaki <henricohosaki@gmail.com>
2025-09-02 21:16:03 -0300

    atualização_route_update

--- src/routes/routesPet.js
+++ src/routes/routesPet.js
@@ -8,6 +8,6 @@ router.get("/pets/", controllerP.FindAll)
 router.get("/pets/:id", controllerP.FindById)
 router.delete("/pets/:id", controllerP.Delete)
 router.post("/pets/", controllerP.Create)
-router.put("pets/:id", controllerP.Update)
+router.put("/pets/:id", controllerP.Update)
 
 module.exports = router;
